73636602d8bfaee1b04003ad77dcf5f1677bde29
Mikel <mikelgarciaurbina@gmail.com>
2022-10-13 17:31:17 +0200

    feat: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+
+const extensions = ['.ts', '.tsx'];
+const EXTERNALS = [];
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      dir: 'dist',
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  ],
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true,
+    }),
+    resolve({
+      extensions,
+      skip: EXTERNALS,
+    }),
+    typescript({
+      tsconfig: 'tsconfig.build.json',
+    }),
+    commonjs(),
+  ],
+  external: EXTERNALS,
+};
