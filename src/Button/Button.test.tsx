bfade42a216fc8b6113f1bd8961967a8955e12c3
Mikel <mikelgarciaurbina@gmail.com>
2022-10-19 11:06:05 +0200

    feat: add testing library

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -6,8 +6,8 @@ describe('Button', () => {
   it('should return 15 for add(10,5)', () => {
     expect(15).toBe(15);
   });
-  // it('renders a text by default', () => {
-  //   const { getByText } = render(<Button />);
-  //   expect(getByText('Lui Button')).toBeInTheDocument();
-  // });
+  it('renders a text by default', () => {
+    const { getByText } = render(<Button />);
+    expect(getByText('Lui Button')).toBeInTheDocument();
+  });
 });
