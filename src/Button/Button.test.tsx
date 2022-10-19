b9aaab8ee1aa39f59465c4f2cb6d51752b9f3791
Mikel <mikelgarciaurbina@gmail.com>
2022-10-19 12:01:38 +0200

    fix: jest test with testing library

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -3,10 +3,7 @@ import { render } from '../test-utils';
 import Button from './Button';
 
 describe('Button', () => {
-  it('should return 15 for add(10,5)', () => {
-    expect(15).toBe(15);
-  });
-  it('renders a text by default', () => {
+  it('renders by default', () => {
     const { getByText } = render(<Button />);
     expect(getByText('Lui Button')).toBeInTheDocument();
   });
