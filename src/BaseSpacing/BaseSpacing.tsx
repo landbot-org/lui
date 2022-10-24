9832a51fc3b6e407d9b7f8c9eb1904f2f9570ce0
Mikel <mikelgarciaurbina@gmail.com>
2022-10-24 12:17:14 +0200

    fix: base spacing unit LAN-13169

--- src/BaseSpacing/BaseSpacing.tsx
+++ src/BaseSpacing/BaseSpacing.tsx
@@ -7,95 +7,95 @@ const BaseSpacing = styled.div<BaseSpacingProps>`
   ${({ m }) =>
     m === undefined
       ? css`
-          margin: 0px;
+          margin: 0rem;
         `
       : css`
-          margin: ${({ theme }) => pxToRem(theme.base_spacing * m)}px;
+          margin: ${({ theme }) => pxToRem(theme.base_spacing * m)}rem;
         `}
 
   ${({ mt }) =>
     mt !== undefined &&
     css`
-      margin-top: ${({ theme }) => pxToRem(theme.base_spacing * mt)}px;
+      margin-top: ${({ theme }) => pxToRem(theme.base_spacing * mt)}rem;
     `}
 
   ${({ mr }) =>
     mr !== undefined &&
     css`
-      margin-right: ${({ theme }) => pxToRem(theme.base_spacing * mr)}px;
+      margin-right: ${({ theme }) => pxToRem(theme.base_spacing * mr)}rem;
     `}
 
   ${({ mb }) =>
     mb !== undefined &&
     css`
-      margin-bottom: ${({ theme }) => pxToRem(theme.base_spacing * mb)}px;
+      margin-bottom: ${({ theme }) => pxToRem(theme.base_spacing * mb)}rem;
     `}
 
   ${({ ml }) =>
     ml !== undefined &&
     css`
-      margin-left: ${({ theme }) => pxToRem(theme.base_spacing * ml)}px;
+      margin-left: ${({ theme }) => pxToRem(theme.base_spacing * ml)}rem;
     `}
 
   ${({ mx }) =>
     mx !== undefined &&
     css`
-      margin-right: ${({ theme }) => pxToRem(theme.base_spacing * mx)}px;
-      margin-left: ${({ theme }) => pxToRem(theme.base_spacing * mx)}px;
+      margin-right: ${({ theme }) => pxToRem(theme.base_spacing * mx)}rem;
+      margin-left: ${({ theme }) => pxToRem(theme.base_spacing * mx)}rem;
     `}
 
   ${({ my }) =>
     my !== undefined &&
     css`
-      margin-top: ${({ theme }) => pxToRem(theme.base_spacing * my)}px;
-      margin-bottom: ${({ theme }) => pxToRem(theme.base_spacing * my)}px;
+      margin-top: ${({ theme }) => pxToRem(theme.base_spacing * my)}rem;
+      margin-bottom: ${({ theme }) => pxToRem(theme.base_spacing * my)}rem;
     `}
 
   ${({ p }) =>
     p === undefined
       ? css`
-          padding: 0px;
+          padding: 0rem;
         `
       : css`
-          padding: ${({ theme }) => pxToRem(theme.base_spacing * p)}px;
+          padding: ${({ theme }) => pxToRem(theme.base_spacing * p)}rem;
         `}
 
   ${({ pt }) =>
     pt !== undefined &&
     css`
-      padding-top: ${({ theme }) => pxToRem(theme.base_spacing * pt)}px;
+      padding-top: ${({ theme }) => pxToRem(theme.base_spacing * pt)}rem;
     `}
 
   ${({ pr }) =>
     pr !== undefined &&
     css`
-      padding-right: ${({ theme }) => pxToRem(theme.base_spacing * pr)}px;
+      padding-right: ${({ theme }) => pxToRem(theme.base_spacing * pr)}rem;
     `}
 
   ${({ pb }) =>
     pb !== undefined &&
     css`
-      padding-bottom: ${({ theme }) => pxToRem(theme.base_spacing * pb)}px;
+      padding-bottom: ${({ theme }) => pxToRem(theme.base_spacing * pb)}rem;
     `}
 
   ${({ pl }) =>
     pl !== undefined &&
     css`
-      padding-left: ${({ theme }) => pxToRem(theme.base_spacing * pl)}px;
+      padding-left: ${({ theme }) => pxToRem(theme.base_spacing * pl)}rem;
     `}
 
   ${({ px }) =>
     px !== undefined &&
     css`
-      padding-right: ${({ theme }) => pxToRem(theme.base_spacing * px)}px;
-      padding-left: ${({ theme }) => pxToRem(theme.base_spacing * px)}px;
+      padding-right: ${({ theme }) => pxToRem(theme.base_spacing * px)}rem;
+      padding-left: ${({ theme }) => pxToRem(theme.base_spacing * px)}rem;
     `}
 
   ${({ py }) =>
     py !== undefined &&
     css`
-      padding-top: ${({ theme }) => pxToRem(theme.base_spacing * py)}px;
-      padding-bottom: ${({ theme }) => pxToRem(theme.base_spacing * py)}px;
+      padding-top: ${({ theme }) => pxToRem(theme.base_spacing * py)}rem;
+      padding-bottom: ${({ theme }) => pxToRem(theme.base_spacing * py)}rem;
     `}
 `;
 
