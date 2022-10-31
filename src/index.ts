51d74013c1d927337795078b3519fc3bbe251b04
Mikel <mikelgarciaurbina@gmail.com>
2022-10-31 17:33:26 +0100

    fix: export theme in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Box } from './Box';
 export { Button } from './Button';
 export { Typography } from './Typography';
+export { theme } from './shared/theme';
+export { Theme } from './shared/theme.types';
