94eebb0bad577b7457b5c101204f166cb4df3404
David Gomez <david.gomez@landbot.io>
2022-12-12 09:15:25 +0100

    fix(icon): add space

--- src/Icon/types.ts
+++ src/Icon/types.ts
@@ -4,6 +4,7 @@ import React from 'react';
 import { ColorsTypes } from '../shared/theme.types';
 
 type Size = '0.5x' | '1x' | '1.25x' | '1.5x' | '2x' | '2.5x' | '3x' | '4x' | '5x';
+
 export interface IconProps extends React.SVGProps<SVGSVGElement> {
   icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
   size: Size;
