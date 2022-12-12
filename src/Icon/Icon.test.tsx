45feb8f6bd94b00aca7745c8987d4e4e5c1359db
David Gomez <david.gomez@landbot.io>
2022-12-12 10:12:52 +0100

    test(icon): add test for icon with different attributes

--- src/Icon/Icon.test.tsx
+++ src/Icon/Icon.test.tsx
@@ -8,4 +8,9 @@ describe('Icon', () => {
     const { container } = render(<Icon icon={calendly} size="1x" color="blue.main" />);
     expect(container.querySelector('svg')).toBeInTheDocument();
   });
+  it('should render svg with different height and width', () => {
+    const { container } = render(<Icon icon={calendly} size="1x" height={100} width={100} />);
+    expect(container.querySelector('svg')).toHaveAttribute('height', '100');
+    expect(container.querySelector('svg')).toHaveAttribute('width', '100');
+  });
 });
