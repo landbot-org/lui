986c64bfb3216721f8255638c51518d0298afdf1
David Gomez <david.gomez@landbot.io>
2022-12-15 17:20:38 +0100

    fix(icon): removing unused interface

--- src/Icon/types.ts
+++ src/Icon/types.ts
@@ -1,6 +1,4 @@
 import React from 'react';
-import { IconDefinition } from '@fortawesome/fontawesome-common-types';
-import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 import { ColorsTypes } from '../shared/theme.types';
 
 export type Size = '0.5x' | '1x' | '1.25x' | '1.5x' | '2x' | '2.5x' | '3x' | '4x' | '5x';
@@ -8,10 +6,5 @@ export type Size = '0.5x' | '1x' | '1.25x' | '1.5x' | '2x' | '2.5x' | '3x' | '4x
 export interface IconProps extends React.SVGProps<SVGSVGElement> {
   color?: ColorsTypes | 'inherit';
   icon: JSX.Element;
-  size?: Size;
-}
-
-export interface FAIconProps extends Omit<FontAwesomeIconProps, 'size'> {
-  icon: IconDefinition;
   size?: Size;
 }
