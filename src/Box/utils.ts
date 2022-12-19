ad460107d2a942509fb049bda0774e6fc035c197
eduardorg <eduardo.ruiz@helloumi.com>
2022-12-19 08:50:42 +0100

    fix: review

--- src/Box/utils.ts
+++ src/Box/utils.ts
@@ -4,48 +4,15 @@ import { Theme } from '../shared/theme.types';
 import { BORDER_RADIUS_MAPPING, BOX_SHADOW_MAPPING } from './constants';
 import { StyledBoxProps } from './types';
 
-export const getElevationStyles = ({ $elevation = 0 }: StyledBoxProps) => {
-  return {
-    0: css`
-      box-shadow: ${BOX_SHADOW_MAPPING[0]};
-    `,
-    1: css`
-      box-shadow: ${BOX_SHADOW_MAPPING[1]};
-    `,
-    2: css`
-      box-shadow: ${BOX_SHADOW_MAPPING[2]};
-    `,
-    3: css`
-      box-shadow: ${BOX_SHADOW_MAPPING[3]};
-    `,
-    4: css`
-      box-shadow: ${BOX_SHADOW_MAPPING[4]};
-    `,
-  }[$elevation];
-};
+export const getElevationStyles = ({ $elevation = 0 }: StyledBoxProps) =>
+  css`
+    box-shadow: ${BOX_SHADOW_MAPPING[$elevation]};
+  `;
 
-export const getRadiusStyles = ({ $radius = 0 }: StyledBoxProps) => {
-  return {
-    0: css`
-      border-radius: ${BORDER_RADIUS_MAPPING[0]}px;
-    `,
-    1: css`
-      border-radius: ${BORDER_RADIUS_MAPPING[1]}px;
-    `,
-    2: css`
-      border-radius: ${BORDER_RADIUS_MAPPING[2]}px;
-    `,
-    3: css`
-      border-radius: ${BORDER_RADIUS_MAPPING[3]}px;
-    `,
-    4: css`
-      border-radius: ${BORDER_RADIUS_MAPPING[4]}px;
-    `,
-    5: css`
-      border-radius: ${BORDER_RADIUS_MAPPING[5]}px;
-    `,
-  }[$radius];
-};
+export const getRadiusStyles = ({ $radius = 0 }: StyledBoxProps) =>
+  css`
+    border-radius: ${BORDER_RADIUS_MAPPING[$radius]}px;
+  `;
 
 export const getBorderStyles = ({
   $border = 0,
