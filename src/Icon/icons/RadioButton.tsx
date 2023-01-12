84369bc676adcda29efbaf63a79028bffd222ba6
David Gomez <david.gomez@landbot.io>
2023-01-12 14:55:12 +0100

    feat(icons): add more icons

---
+++ src/Icon/icons/RadioButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const RadioButton = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M20 40C31.0457 40 40 31.0457 40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 31.0457 8.9543 40 20 40Z"
+      fill="#226699"
+    />
+    <path
+      d="M20 28.8891C24.9092 28.8891 28.8889 24.9094 28.8889 20.0002C28.8889 15.091 24.9092 11.1113 20 11.1113C15.0908 11.1113 11.1111 15.091 11.1111 20.0002C11.1111 24.9094 15.0908 28.8891 20 28.8891Z"
+      fill="#BBDDF5"
+    />
+  </svg>
+);
