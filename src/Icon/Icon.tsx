7f7136939e43cbf9f6c3fe61b7776a836a300182
David Gomez <david.gomez@landbot.io>
2023-01-13 09:59:11 +0100

    fix(icon): add classname to icon container

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyledIconContainer } from './Icon.styles';
 import { GRID_AREA, IconProps } from './types';
 
-export const Icon = ({ icon, size = '1x', gridArea = GRID_AREA.NONE, color = 'inherit' }: IconProps) => {
+export const Icon = ({ icon, size = '1x', gridArea = GRID_AREA.NONE, color = 'inherit', className }: IconProps) => {
   return (
-    <StyledIconContainer $color={color} $gridArea={gridArea} $size={size}>
+    <StyledIconContainer $color={color} $gridArea={gridArea} $size={size} className={className}>
       {icon}
     </StyledIconContainer>
   );
