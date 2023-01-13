6d75f14676b4591270047947ef33179f7293f0f6
David Gomez <david.gomez@landbot.io>
2023-01-13 10:21:59 +0100

    fix(icon): add other props to icon container

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { StyledIconContainer } from './Icon.styles';
 import { GRID_AREA, IconProps } from './types';
 
-export const Icon = ({ icon, size = '1x', gridArea = GRID_AREA.NONE, color = 'inherit', className }: IconProps) => {
-  return (
-    <StyledIconContainer $color={color} $gridArea={gridArea} $size={size} className={className}>
-      {icon}
-    </StyledIconContainer>
-  );
-};
+export const Icon = ({ icon, size = '1x', gridArea = GRID_AREA.NONE, color = 'inherit', ...rest }: IconProps) => (
+  <StyledIconContainer $color={color} $gridArea={gridArea} $size={size} {...rest}>
+    {icon}
+  </StyledIconContainer>
+);
