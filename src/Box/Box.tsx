6355ee308562cc3f2751c9840a0ce155f02956c0
eduardorg <eduardo.ruiz@helloumi.com>
2023-02-06 16:05:11 +0100

    feat(Box): forward ref

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -2,39 +2,47 @@ import React from 'react';
 import { BoxProps } from './types';
 import { BoxStyled } from './Box.styles';
 
-export const Box = ({
-  alignItems,
-  as = 'div',
-  backgroundColor,
-  border,
-  borderColor,
-  children,
-  className,
-  display = 'block',
-  elevation = 0,
-  flexDirection,
-  flexWrap,
-  gap,
-  justifyContent,
-  radius = 0,
-  ...other
-}: BoxProps) => (
-  <BoxStyled
-    as={as}
-    className={className}
-    $alignItems={alignItems}
-    $backgroundColor={backgroundColor}
-    $border={border}
-    $borderColor={borderColor}
-    $display={display}
-    $elevation={elevation}
-    $flexDirection={flexDirection}
-    $flexWrap={flexWrap}
-    $gap={gap}
-    $justifyContent={justifyContent}
-    $radius={radius}
-    {...other}
-  >
-    {children}
-  </BoxStyled>
+export const Box = React.forwardRef<HTMLElement, BoxProps>(
+  (
+    {
+      alignItems,
+      as = 'div',
+      backgroundColor,
+      border,
+      borderColor,
+      children,
+      className,
+      display = 'block',
+      elevation = 0,
+      flexDirection,
+      flexWrap,
+      gap,
+      justifyContent,
+      radius = 0,
+      ...other
+    },
+    ref
+  ) => (
+    <BoxStyled
+      as={as}
+      className={className}
+      $alignItems={alignItems}
+      $backgroundColor={backgroundColor}
+      $border={border}
+      $borderColor={borderColor}
+      $display={display}
+      $elevation={elevation}
+      $flexDirection={flexDirection}
+      $flexWrap={flexWrap}
+      $gap={gap}
+      $justifyContent={justifyContent}
+      $radius={radius}
+      {...other}
+      ref={ref}
+    >
+      {children}
+    </BoxStyled>
+  )
 );
+
+Box.displayName = 'Box';
