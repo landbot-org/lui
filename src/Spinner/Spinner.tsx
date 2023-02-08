38dda98fc9a8deb815609e23adb61022d350aadc
Santiago Moncada <santiago.m.a@hotmail.com>
2023-02-08 16:21:13 +0100

    fix(Spinner): added aria labels

--- src/Spinner/Spinner.tsx
+++ src/Spinner/Spinner.tsx
@@ -9,7 +9,7 @@ import { NUMBER_OF_PILLS } from './constants';
 const arrayPills = Array.from({ length: NUMBER_OF_PILLS }, (_, i) => i);
 
 export const Spinner = ({ size = 'M', color = 'blue.main', spinnerColor = 'neutral.300' }: SpinnerProps) => (
-  <SpinnerStyled $size={size}>
+  <SpinnerStyled aria-busy="true" aria-live="polite" $size={size}>
     {arrayPills.map((n) => (
       <PillWrapperStyled key={n} $nth={n}>
         <PillStyled $nth={n} $color={color} $spinnerColor={spinnerColor} />
