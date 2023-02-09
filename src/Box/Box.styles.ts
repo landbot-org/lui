6a1fc40d34df119a30ff4df509f575ab332759fe
eduardorg <eduardo.ruiz@helloumi.com>
2023-02-09 10:02:19 +0100

    feat(Box): more flexbox props

--- src/Box/Box.styles.ts
+++ src/Box/Box.styles.ts
@@ -7,9 +7,12 @@ import { getBackgroundStyles, getBorderStyles, getElevationStyles, getRadiusStyl
 export const BoxStyled = styled(BaseSpacing)<StyledBoxProps>`
   align-items: ${({ $alignItems }) => $alignItems};
   display: ${({ $display }) => $display};
+  flex-basis: ${({ $flexBasis }) => $flexBasis};
   flex-direction: ${({ $flexDirection }) => $flexDirection};
-  justify-content: ${({ $justifyContent }) => $justifyContent};
+  flex-grow: ${({ $flexGrow }) => $flexGrow};
+  flex-shrink: ${({ $flexShrink }) => $flexShrink};
   flex-wrap: ${({ $flexWrap }) => $flexWrap};
+  justify-content: ${({ $justifyContent }) => $justifyContent};
   gap: ${({ $gap }) => $gap && `${$gap}px`};
   ${getBackgroundStyles}
   ${getElevationStyles}
