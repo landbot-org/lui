b95e33d4f025b5bfa95f4434a99ef9c0f60cf161
eduardorg <eduardo.ruiz@helloumi.com>
2023-03-21 15:49:51 +0100

    fix(Link): icon style

--- src/Link/Link.styles.ts
+++ src/Link/Link.styles.ts
@@ -5,6 +5,7 @@ import { LinkProps } from './types';
 export const BaseLink = styled.a<LinkProps>`
   color: ${({ theme }: { theme: Theme }) => theme.palette.purple.main};
   display: inline-flex;
+  align-items: center;
   gap: 4px;
   text-decoration: none;
   &:hover {
