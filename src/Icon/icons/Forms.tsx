210e23820fbf8d7558a7ae920c6a9bf59cb83785
Jaime Ferrer <jaime.f@landbot.io>
2023-04-11 13:58:00 +0200

    feat: add icons

---
+++ src/Icon/icons/Forms.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Forms = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="40" height="35" viewBox="0 0 40 35" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <rect x="0.4" y="0.4" width="39.2" height="6.47273" fill="white" stroke="#33405E" strokeWidth="0.8" />
+    <rect x="0.4" y="9.33945" width="39.2" height="6.47273" fill="white" stroke="#33405E" strokeWidth="0.8" />
+    <rect x="0.4" y="18.2794" width="39.2" height="6.47273" fill="white" stroke="#33405E" strokeWidth="0.8" />
+    <rect x="0.4" y="27.2188" width="39.2" height="6.47273" fill="white" stroke="#33405E" strokeWidth="0.8" />
+  </svg>
+);
---
+++ src/Icon/icons/ReplyButtons.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+export const ReplyButtons = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <g clipPath="url(#clip0_2025_2543)">
+      <rect width="40" height="40" fill="#CCD6DD" />
+      <g filter="url(#filter0_d_2025_2543)">
+        <rect x="3.33337" y="23.3335" width="16.6667" height="5" rx="0.4" fill="white" />
+      </g>
+      <g filter="url(#filter1_d_2025_2543)">
+        <rect x="21.6666" y="23.3335" width="15" height="5" rx="0.4" fill="white" />
+      </g>
+      <rect x="3.33337" y="30" width="33.3333" height="5" rx="0.4" fill="white" />
+      <g filter="url(#filter2_d_2025_2543)">
+        <rect x="3.33337" y="6.6665" width="33.3333" height="13.3333" rx="0.5" fill="white" />
+      </g>
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_2025_2543"
+        x="-0.666626"
+        y="23.3335"
+        width="24.6666"
+        height="13"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2025_2543" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2025_2543" result="shape" />
+      </filter>
+      <filter
+        id="filter1_d_2025_2543"
+        x="17.6666"
+        y="23.3335"
+        width="23"
+        height="13"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2025_2543" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2025_2543" result="shape" />
+      </filter>
+      <filter
+        id="filter2_d_2025_2543"
+        x="-0.666626"
+        y="3.6665"
+        width="41.3334"
+        height="21.3335"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.13 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2025_2543" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2025_2543" result="shape" />
+      </filter>
+      <clipPath id="clip0_2025_2543">
+        <rect width="40" height="40" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
