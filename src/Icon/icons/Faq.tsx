30140186a0e785b2c1c164e27b6595b2027733ef
Eduardo Ruiz <EduardoRG@users.noreply.github.com>
2023-05-24 11:04:43 +0200

    feat(Icon): added Faq icon (#78)
    Co-authored-by: eduardorg <eduardo.ruiz@helloumi.com>

---
+++ src/Icon/icons/Faq.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+export const Faq = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="40" height="42" viewBox="0 0 40 42" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <mask
+      id="mask0_3019_8068"
+      style={{ maskType: 'luminance' }}
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      width="40"
+      height="41"
+    >
+      <path d="M40 0.625H0V40.625H40V0.625Z" fill="white" />
+    </mask>
+    <g mask="url(#mask0_3019_8068)">
+      <path
+        d="M5 -0.625C2.24219 -0.625 0 1.61719 0 4.375V26.875C0 29.6327 2.24219 31.875 5 31.875H12.5V38.125C12.5 38.6015 12.7656 39.0312 13.1875 39.2421C13.6094 39.4531 14.1173 39.4062 14.5 39.125L24.164 31.875H35C37.7578 31.875 40 29.6327 40 26.875V4.375C40 1.61719 37.7578 -0.625 35 -0.625H5Z"
+        fill="#4FCCC2"
+      />
+      <g filter="url(#filter0_ddd_3019_8068)">
+        <path
+          d="M7.18506 21.4575V11.8325H13.4001V13.2488H8.94506V15.9438H12.5476V17.3325H8.94506V21.4575H7.18506Z"
+          fill="white"
+        />
+        <path
+          d="M13.0923 21.4575L16.6123 11.8325H18.5923L22.1123 21.4575H20.2423L19.4723 19.23H15.7185L14.9348 21.4575H13.0923ZM16.1998 17.855H18.991L17.5885 13.8538L16.1998 17.855Z"
+          fill="white"
+        />
+        <path
+          d="M27.3116 21.6225C26.3491 21.6225 25.5057 21.4116 24.7816 20.99C24.0666 20.5684 23.5029 19.9863 23.0904 19.2438C22.687 18.4921 22.4854 17.6259 22.4854 16.645C22.4854 15.6641 22.687 14.8025 23.0904 14.06C23.5029 13.3084 24.0666 12.7216 24.7816 12.3C25.5057 11.8783 26.3491 11.6675 27.3116 11.6675C28.2649 11.6675 29.1036 11.8783 29.8279 12.3C30.552 12.7216 31.1157 13.3084 31.5191 14.06C31.9224 14.8025 32.1241 15.6641 32.1241 16.645C32.1241 17.5616 31.9454 18.3821 31.5879 19.1063C31.2304 19.8213 30.7354 20.3896 30.1029 20.8113L32.3304 23.2038H30.1441L28.6316 21.4713C28.2099 21.5721 27.7699 21.6225 27.3116 21.6225ZM27.3116 20.0413C28.2282 20.0413 28.957 19.7388 29.4979 19.1338C30.0479 18.5288 30.3229 17.6991 30.3229 16.645C30.3229 15.5909 30.0479 14.7613 29.4979 14.1563C28.957 13.5513 28.2282 13.2488 27.3116 13.2488C26.3949 13.2488 25.6616 13.5513 25.1116 14.1563C24.5616 14.7613 24.2866 15.5909 24.2866 16.645C24.2866 17.6991 24.5616 18.5288 25.1116 19.1338C25.6616 19.7388 26.3949 20.0413 27.3116 20.0413Z"
+          fill="white"
+        />
+      </g>
+    </g>
+    <defs>
+      <filter
+        id="filter0_ddd_3019_8068"
+        x="-0.814941"
+        y="11.6675"
+        width="41.1453"
+        height="27.5361"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="8" />
+        <feGaussianBlur stdDeviation="4" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_3019_8068" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+        <feBlend mode="normal" in2="effect1_dropShadow_3019_8068" result="effect2_dropShadow_3019_8068" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="2" />
+        <feGaussianBlur stdDeviation="1" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+        <feBlend mode="normal" in2="effect2_dropShadow_3019_8068" result="effect3_dropShadow_3019_8068" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect3_dropShadow_3019_8068" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+);
