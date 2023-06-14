c34800f4a5ebdbdf45d7f904fa3254e540a9d1c3
Jaime Ferrer <jaime.f@landbot.io>
2023-06-14 17:23:25 +0200

    feat: simplify trigger

--- src/Popover/PopoverTrigger.tsx
+++ src/Popover/PopoverTrigger.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ForwardedRef, HTMLProps, ReactNode, forwardRef } from 'react';
 import { usePopoverContext } from './PopoverContext';
 import { useMergeRefs } from '@floating-ui/react';
 
@@ -6,16 +6,19 @@ interface PopoverTriggerProps {
   children: ReactNode;
 }
 
-export const PopoverTrigger = React.forwardRef<HTMLElement, React.HTMLProps<HTMLElement> & PopoverTriggerProps>(
-  function PopoverTrigger({ children, ...props }, propRef) {
-    const context = usePopoverContext();
-    const childrenRef = (children as any).ref;
-    const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+const PopoverTriggerBase = (
+  { children, ...props }: HTMLProps<HTMLElement> & PopoverTriggerProps,
+  propRef: ForwardedRef<HTMLElement>
+) => {
+  const context = usePopoverContext();
+  const childrenRef = (children as any).ref;
+  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
 
-    return (
-      <button ref={ref} type="button" {...context.getReferenceProps(props)}>
-        {children}
-      </button>
-    );
-  }
-);
+  return (
+    <button ref={ref} type="button" {...context.getReferenceProps(props)}>
+      {children}
+    </button>
+  );
+};
+
+export const PopoverTrigger = forwardRef<HTMLElement, HTMLProps<HTMLElement> & PopoverTriggerProps>(PopoverTriggerBase);
