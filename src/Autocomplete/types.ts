f2fa82891d70ba6d43865ce6325fcc0b796b1fdb
David Gomez <david.gomez@landbot.io>
2023-06-20 13:18:27 +0200

    feat(autocomplete): fixes review

--- src/Autocomplete/types.ts
+++ src/Autocomplete/types.ts
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react';
 import { TextFieldProps } from '../TextField/types';
 
-type AutoCompleteItem = { id: string; value: string };
+export interface AutoCompleteItem {
+  id: string;
+  value: string;
+}
 
 export interface AutocompleteProps {
   items: AutoCompleteItem[];
-  placeholder: string;
+  placeholder?: string;
   startAdornment?: ReactNode;
   endAdornment?: ReactNode;
   inputValue?: string;
