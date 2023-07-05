9d38739c558193c58473883cd8c6a97d7a868ab1
Santiago Moncada <santiago.m@landbot.io>
2023-07-05 17:22:59 +0200

    fix(popover): added px to rem

--- src/Popover/Popover.styles.tsx
+++ src/Popover/Popover.styles.tsx
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 import { Box } from '../Box';
 import { Button } from '../Button';
+import { pxToRem } from '../shared/mixins';
 
 export const StyledPopoverContent = styled(Box)`
   background-color: ${({ theme }) => theme.palette.white.main};
-  border: 1px solid ${({ theme }) => theme.palette.neutral[300]};
-  border-radius: 4px;
-  padding: 16px;
+  border: ${pxToRem(1)}rem solid ${({ theme }) => theme.palette.neutral[300]};
+  border-radius: ${pxToRem(4)}rem;
+  padding: ${pxToRem(16)}rem;
 `;
 
 export const StyledCloseButton = styled(Button)`
   padding: 0px;
-  height: 16px;
-  width: 16px;
+  height: ${pxToRem(16)}rem;
+  width: ${pxToRem(16)}rem; ;
 `;
