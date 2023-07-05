145f29fdfdeab122d9e8631b89a2a5ca80955313
Santiago Moncada <santiago.m@landbot.io>
2023-07-05 13:20:01 +0200

    fix(popover): added styled comp

--- src/Popover/Popover.styles.tsx
+++ src/Popover/Popover.styles.tsx
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 import { Box } from '../Box';
+import { Button } from '../Button';
 
 export const StyledPopoverContent = styled(Box)`
   background-color: ${({ theme }) => theme.palette.white.main};
   border: 1px solid ${({ theme }) => theme.palette.neutral[300]};
   border-radius: 4px;
   padding: 16px;
+`;
+
+export const StyledCloseButton = styled(Button)`
+  padding: 0px;
+  height: 16px;
+  width: 16px;
 `;
--- src/Popover/PopoverContent.tsx
+++ src/Popover/PopoverContent.tsx
@@ -2,11 +2,11 @@ import React, { ForwardedRef, HTMLProps, forwardRef } from 'react';
 import { usePopoverContext } from './PopoverContext';
 import { FloatingFocusManager, FloatingPortal, useMergeRefs, FloatingArrow } from '@floating-ui/react';
 import { theme } from '../shared/theme';
-import { StyledPopoverContent } from './Popover.styles';
+import { StyledPopoverContent, StyledCloseButton } from './Popover.styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Button } from '../Button';
 import { Box } from '../Box';
+
 const PopoverContentBase = ({ style, ...props }: HTMLProps<HTMLDivElement>, propRef: ForwardedRef<HTMLDivElement>) => {
   {
     const {
@@ -37,12 +37,11 @@ const PopoverContentBase = ({ style, ...props }: HTMLProps<HTMLDivElement>, prop
           >
             {hasCloseButton && onOpenChange && (
               <Box display="flex" justifyContent="flex-end" pb={1}>
-                <Button
+                <StyledCloseButton
                   startIcon={<FontAwesomeIcon icon={faXmark} />}
                   variant="text"
                   color="blue.main"
                   onClick={() => onOpenChange(false)}
-                  style={{ padding: '0px', height: '16px', width: '16px' }}
                 />
               </Box>
             )}
