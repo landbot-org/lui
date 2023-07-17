50af8b1f253866497ae46b3079fb3be14aba11cd
eduardorg <eduardo.ruiz@helloumi.com>
2023-07-17 15:33:54 +0200

    docs(Dialog): more stories

--- src/Dialog/Dialog.stories.tsx
+++ src/Dialog/Dialog.stories.tsx
@@ -68,7 +68,7 @@ export const WithFooter: ComponentStory<typeof Dialog> = (args: DialogProps) => 
               <Button variant="outlined" color="blue.main">
                 CANCEL
               </Button>
-              <Button variant="contained">SAVE</Button>
+              <Button>SAVE</Button>
             </Box>
           </Box>
         </DialogContent>
@@ -99,7 +99,7 @@ export const WithHugeContent: ComponentStory<typeof Dialog> = (args: DialogProps
               <Button variant="outlined" color="blue.main">
                 CANCEL
               </Button>
-              <Button variant="contained">SAVE</Button>
+              <Button>SAVE</Button>
             </Box>
           </Box>
         </DialogContent>
@@ -109,4 +109,62 @@ export const WithHugeContent: ComponentStory<typeof Dialog> = (args: DialogProps
 };
 WithHugeContent.args = {
   width: 3800,
+};
+
+export const WithFixedHeaderAndFooter: ComponentStory<typeof Dialog> = (args: DialogProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dialog {...args}>
+        <DialogContent>
+          <Box display="flex" flexDirection="column" p={4} style={{ height: '95vh' }}>
+            <Typography variant="h5">Dialog</Typography>
+            <Box my={2}>
+              <Divider />
+            </Box>
+            <Box flexGrow={1} style={{ overflow: 'auto' }}>
+              <Box mb={2} backgroundColor="orange.200" style={{ height: 100 }} />
+              <Box mb={2} backgroundColor="orange.400" style={{ height: 100 }} />
+              <Box mb={2} backgroundColor="orange.600" style={{ height: 100 }} />
+              <Box mb={2} backgroundColor="teal.200" style={{ height: 100 }} />
+              <Box mb={2} backgroundColor="teal.400" style={{ height: 100 }} />
+              <Box mb={2} backgroundColor="teal.600" style={{ height: 100 }} />
+            </Box>
+            <Box my={2}>
+              <Divider />
+            </Box>
+            <Box display="flex" justifyContent="flex-end" gap={8}>
+              <Button variant="outlined" color="blue.main">
+                CANCEL
+              </Button>
+              <Button>SAVE</Button>
+            </Box>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </ThemeProvider>
+  );
+};
+
+export const WithCustomContent: ComponentStory<typeof Dialog> = (args: DialogProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dialog {...args}>
+        <DialogContent>
+          <Box backgroundColor="warning.main" p={6} display="flex" justifyContent="center" flexDirection="column">
+            <Typography color="neutral.800" variant="h3" mb={3} style={{ textAlign: 'center' }}>
+              Dialog
+            </Typography>
+            <Box display="grid" gap={16} style={{ gridTemplateColumns: '1fr 1fr 1fr' }}>
+              <Box radius={2} backgroundColor="warning.dark" style={{ height: 150, cursor: 'pointer' }}></Box>
+              <Box radius={2} backgroundColor="warning.dark" style={{ height: 150, cursor: 'pointer' }}></Box>
+              <Box radius={2} backgroundColor="warning.dark" style={{ height: 150, cursor: 'pointer' }}></Box>
+            </Box>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </ThemeProvider>
+  );
+};
+WithCustomContent.args = {
+  width: 600,
 };
