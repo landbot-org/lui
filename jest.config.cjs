cb1d8a3013529b06ce23c73ace86afc3f512b833
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2024-01-23 14:56:57 +0100

    feat: add name mappers for jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,11 @@ module.exports = {
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/.jest/__mocks__/svgMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.jest/__mocks__/fileMock.js',
+  },
   testMatch: ['**/src/**/*.test.+(ts|tsx|js|jsx)'],
   collectCoverageFrom: [
     '**/*.{ts|tsx|js|jsx}',
