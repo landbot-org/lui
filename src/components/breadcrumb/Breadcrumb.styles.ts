59b93dd288ff76eddcf72a9b3630ee315e73b544
David Gomez <david.gomez@landbot.io>
2024-01-31 11:23:49 +0100

    feat(components): create breadcrumb component

--- src/components/breadcrumb/Breadcrumb.styles.ts
+++ src/components/breadcrumb/Breadcrumb.styles.ts
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
 import { ColorsTypes } from '../../shared/theme.types';
 import { Typography } from '../typography';
