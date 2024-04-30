311ca09f2cfa61558bd12a5f064db67c1b721e95
David <davidgomezrequeijo@gmail.com>
2024-04-30 14:46:32 +0200

    feat(components): add resize to textarea (#168)

--- src/components/text-area/TextArea.styles.ts
+++ src/components/text-area/TextArea.styles.ts
@@ -10,10 +10,12 @@ import {
 } from '../../shared/inputStyles';
 import { pxToRem } from '../../shared/mixins';
 import type { Theme } from '../../shared/theme.types';
+import { Resize } from './TextArea.types';
 
 interface StyledTextAreaProps {
   $disabled?: boolean;
   $error?: boolean;
+  $resize: Resize;
   theme: Theme;
 }
 
@@ -27,4 +29,5 @@ export const StyledTextArea = styled.textarea<StyledTextAreaProps>`
   flex-grow: 1;
   min-height: ${pxToRem(80)}rem;
   padding: ${pxToRem(8)}rem ${pxToRem(16)}rem;
+  resize: ${({ $resize }) => $resize};
 `;
--- src/components/text-area/TextArea.types.ts
+++ src/components/text-area/TextArea.types.ts
@@ -1,9 +1,12 @@
 import { TextareaHTMLAttributes } from 'react';
 
+export type Resize = 'none' | 'both' | 'horizontal' | 'vertical';
+
 export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label?: string;
   description?: string;
   error?: boolean;
   helperText?: string;
   fullHeight?: boolean;
+  resize: Resize;
 }
