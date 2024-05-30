0bb102dfc45bc2437db24ae448256e1c18a8c60b
Raúl Ocaña <raul.o@landbot.io>
2024-05-30 21:58:26 +0700

    feat: add input field

--- src/components/chat/layout/ChatInput.styles.ts
+++ src/components/chat/layout/ChatInput.styles.ts
@@ -1,20 +1,48 @@
 import { styled } from 'styled-components';
+import { Theme } from "../../../shared/theme.types";
 
 export const StyledChatInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 25px;
+  
+  //background: red;
+`;
+
+export const StyleInputGroup = styled.div<{ $error: boolean }>`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  margin: 25px;
-  border: 1px solid #e0e0e0;
+  padding: 16px;
+  border: 1px solid ${({ $error, theme } : { $error: boolean, theme: Theme}) => $error ? theme.palette.error.main : 'transparent'};;
+  border-radius: 12px;
+  &:hover {
+    border: 1px solid ${({ $error, theme } : { $error: boolean, theme: Theme}) => $error ? theme.palette.error.main : theme.palette.neutral[400]};
+  }
+  &:focus-within {
+    border: 1px solid ${({ $error, theme } : { $error: boolean, theme: Theme}) => $error ? theme.palette.error.main : theme.palette.purple.main};
+  }
 `;
 
-export const StyledInput = styled.input`
+export const StyledHelperText = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  gap: 4px;
+`;
+
+export const StyledTextArea = styled.textarea`
+  font-family: ${({ theme }) => theme.typography.font.primary};
+  font-size: 16px;
+  width: 100%;
+  max-height: 100px;
+  overflow: auto;
   margin: 0;
-  font-family: inherit;
-  font-size: inherit;
   line-height: 1.5;
-  box-shadow: none;
+  background: transparent;
   border: none;
   outline: none;
+  resize: none;
+  
 `;
