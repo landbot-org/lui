dc8b4645c9c797730bc0978ce3cb2277191f5171
David Gomez <david.gomez@landbot.io>
2024-09-13 09:20:13 +0200

    feat(components): create toast component

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -2,6 +2,5 @@ import 'styled-components';
 import { Theme } from '../shared/theme.types';
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  export interface DefaultTheme extends Theme {}
+  export type DefaultTheme = Theme;
 }
