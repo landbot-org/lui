53056d935f01cf64590de70cf06dd5f0a5a9bf0e
David <davidgomezrequeijo@gmail.com>
2024-09-16 08:22:56 +0200

    fix(typecheck): add typecheck to ci (#190)
    * fix(typecheck): add typecheck to ci
    
    * fix(typecheck): add typecheck to ci

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -2,5 +2,6 @@ import 'styled-components';
 import { Theme } from '../shared/theme.types';
 
 declare module 'styled-components' {
-  export type DefaultTheme = Theme;
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface DefaultTheme extends Theme {}
 }
