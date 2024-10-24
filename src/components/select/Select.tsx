946b2f20a2271936362ad40e3ecbf23c8ed257c7
JaimeFLandbot <127306250+JaimeFLandbot@users.noreply.github.com>
2024-10-24 15:58:39 +0200

    Improve table footer styles (#202)
    * feat: maxHeight for table footer
    
    * feat: maxHeight for table footer

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -122,31 +122,33 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
                   },
                 })}
               >
-                <Box border={1} radius={1} p={1} backgroundColor="white.main">
-                  {items.length > 0 ? (
-                    items.map((item, index) => (
-                      <SelectItem
-                        key={item.value}
-                        {...getItemProps({
-                          key: item.value,
-                          ref(node) {
-                            listRef.current[index] = node;
-                          },
-                          onClick() {
-                            handleSelectItem(item);
-                            refs.domReference.current?.focus();
-                          },
-                        })}
-                        active={activeIndex === index || value === item.value}
-                      >
-                        {item.label}
-                      </SelectItem>
-                    ))
-                  ) : (
-                    <Typography variant="text12" color="neutral.400" p={1}>
-                      {noResults}
-                    </Typography>
-                  )}
+                <Box style={styles?.optionsContainer}>
+                  <Box border={1} radius={1} p={1} backgroundColor="white.main">
+                    {items.length > 0 ? (
+                      items.map((item, index) => (
+                        <SelectItem
+                          key={item.value}
+                          {...getItemProps({
+                            key: item.value,
+                            ref(node) {
+                              listRef.current[index] = node;
+                            },
+                            onClick() {
+                              handleSelectItem(item);
+                              refs.domReference.current?.focus();
+                            },
+                          })}
+                          active={activeIndex === index || value === item.value}
+                        >
+                          {item.label}
+                        </SelectItem>
+                      ))
+                    ) : (
+                      <Typography variant="text12" color="neutral.400" p={1}>
+                        {noResults}
+                      </Typography>
+                    )}
+                  </Box>
                 </Box>
               </div>
             </FloatingFocusManager>
--- src/components/select/Select.types.ts
+++ src/components/select/Select.types.ts
@@ -24,5 +24,6 @@ export interface SelectProps {
   ariaLabel?: string;
   styles?: {
     input?: CSSProperties;
+    optionsContainer?: CSSProperties;
   };
 }
