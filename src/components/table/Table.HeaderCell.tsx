66604a84422a5ff51a2050b5bacb6d952eafab03
SaidLandbot <said.a@landbot.io>
2025-03-04 15:56:45 +0100

    feat: Allow pass a menu actions to the table header (#229)
    * feat: Allow pass a menu actions to the table header
    
    * feat: unnecesary undefined check

--- src/components/table/Table.HeaderCell.tsx
+++ src/components/table/Table.HeaderCell.tsx
@@ -1,10 +1,12 @@
+import { ReactNode } from 'react';
 import { StyledCell, StyledTableText } from './Table.styles';
 import { CellProps } from './Table.types';
 
-export const TableHeaderCell = ({ size, children, flexGrow }: CellProps) => {
+export const TableHeaderCell = ({ size, children, flexGrow, menuActions }: CellProps & { menuActions?: ReactNode }) => {
   return (
     <StyledCell $width={size} flexGrow={flexGrow}>
       <StyledTableText>{children}</StyledTableText>
+      {menuActions}
     </StyledCell>
   );
 };
