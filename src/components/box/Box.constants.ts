7443865e4df33cd77fd7447d099f6b123dad2826
Said Atrahouch <said.a@landbot.io>
2025-03-18 15:37:49 +0100

    feat: sync shadows of layer 2 with figma value

--- src/components/box/Box.constants.ts
+++ src/components/box/Box.constants.ts
@@ -3,7 +3,7 @@ import { ElevationType, RadiusType } from './Box.types';
 export const BOX_SHADOW_MAPPING: Record<ElevationType, string> = {
   0: 'none',
   1: '0px 1px 1px rgba(0, 0, 0, 0.08), 0px 2px 2px 1px rgba(0, 0, 0, 0.08)',
-  2: '0px 2px 2px rgba(0, 0, 0, 0.08), 0px 4px 4px rgba(0, 0, 0, 0.08), 0px 8px 8px rgba(0, 0, 0, 0.04)',
+  2: '0px 2px 2px 0px rgba(0, 0, 0, 0.08), 0px 4px 4px 0px rgba(0, 0, 0, 0.08), 0px 8px 8px 0px rgba(0, 0, 0, 0.04)',
   3: '0px 4px 4px rgba(0, 0, 0, 0.04), 0px 8px 8px rgba(0, 0, 0, 0.06), 0px 16px 16px rgba(0, 0, 0, 0.08)',
   4: '0px 4px 4px rgba(0, 0, 0, 0.04), 0px 8px 8px rgba(0, 0, 0, 0.08), 0px 16px 16px rgba(0, 0, 0, 0.08), 0px 32px 32px rgba(0, 0, 0, 0.08)',
 };
