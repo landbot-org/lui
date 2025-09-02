a396471980fefed9536340f06f9636ec9b636aa7
EduardoRG <eduruizgonza@gmail.com>
2025-09-02 08:15:11 +0200

    feat: inputs improvements

--- src/components/menu/MenuButton.tsx
+++ src/components/menu/MenuButton.tsx
@@ -3,6 +3,6 @@ import { Button, ButtonProps } from '../button';
 
 export const MenuButton = forwardRef<HTMLElement, ButtonProps>(
   ({ color = 'blue.main', textAlign = 'left', variant: _, ...props }, ref) => {
-    return <Button ref={ref} variant="menu-item" textAlign={textAlign} color={color} {...props} />;
+    return <Button ref={ref} variant="menu-item" textAlign={textAlign} color={color} role="menuitem" {...props} />;
   },
 );
