5c84e426059171298ba78821d88277d8dafa2bd0
EduardoRG <eduruizgonza@gmail.com>
2025-09-30 09:18:12 +0200

    feat: update popovercontent border color

--- src/components/menu/Menu.styles.ts
+++ src/components/menu/Menu.styles.ts
@@ -2,8 +2,6 @@ import { styled } from 'styled-components';
 import { Box } from '../box';
 
 export const StyledContainer = styled(Box).attrs({
-  backgroundColor: 'white.main',
-  radius: 1,
   display: 'flex',
   flexDirection: 'column',
   gap: 4,
