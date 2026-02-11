dc5409a154e269d345d0eb5f3bfb78fab141e367
EduardoRG <eduruizgonza@gmail.com>
2026-02-11 09:36:02 +0100

    feat: render FormHelperText as div

--- src/components/form-helper-text/FormHelperText.tsx
+++ src/components/form-helper-text/FormHelperText.tsx
@@ -2,6 +2,8 @@ import { styled } from 'styled-components';
 import { pxToRem } from '../../shared/mixins';
 import { Typography } from '../typography';
 
-export const FormHelperText = styled(Typography)`
+export const FormHelperText = styled(Typography).attrs({
+  forwardedAs: 'div',
+})`
   margin-top: ${pxToRem(4)}rem;
 `;
